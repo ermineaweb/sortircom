f47da9c73e2f4e009b075ebf5e6e8d4e20a4dd9b
ermineaweb <ermineaweb@gmail.com>
2019-11-19 14:10:06 +0100

    crud event, crud place

---
+++ src/Form/EventType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('start')
+            ->add('end')
+            ->add('limitdate')
+            ->add('maxsize')
+            ->add('description')
+            ->add('users')
+            ->add('creator')
+            ->add('status')
+            ->add('place')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
