56ad66c2532ae99c0e8c9350d3d2a24b7d4b4357
ermineaweb <ermineaweb@gmail.com>
2019-11-26 11:28:38 +0100

    ajax

--- public/assets/js/places.js
+++ public/assets/js/places.js
@@ -1,3 +1,7 @@
+/*
+Permet de récupérer la liste des places d'une ville via AJAX,
+puis les coordonnées de la place sont mises à jours
+ */
 $(document).ready(() => {
     // si on veut modifier les places récupérées, on changera la requête qui est exécutée dans le controlle sur cette adresse :
     const url = "http://localhost/sortircom/sortircom/public/index.php/place/api";
@@ -10,15 +14,21 @@ $(document).ready(() => {
 
     // à chaque changement de la ville, on met a jour le champ select place
     $("#city").change(() => {
-        // parcours du résultat json
-        console.log(places.filter(p => p.city == $("#city").val()));
-        // on met a jour le champ select places
-        // TODO
+        // on récupère les places associées à la nouvelle ville sélectionnée
+        let newPlaces = places.filter(p => p.city == $("#city").val());
+        let $el = $("#event_place");
+        // on supprime les anciennes places
+        $el.empty();
+        $.each(newPlaces, (key, val) => {
+            $el.append($("<option></option>").attr("value", val.id).text(val.name));
+        });
     });
 
     // à chaque changement de place, on met à jour les coordonnées de la place (address, latitude, longitude)
     $("#event_place").change(() => {
-        console.log("hibhb" + $("#event_place").val());
+        let place = places.find(p => p.id == $("#event_place").val());
+        $("#address").html(place.address);
+        $("#latitude").html(place.latitude);
+        $("#longitude").html(place.longitude);
     });
-
 });
